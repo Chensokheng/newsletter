2bd05b9c00e709c37768e9f5c6e452c90e5fa3d1
Chensokheng <52232579+Chensokheng@users.noreply.github.com>
2024-04-10 22:41:37 +0700

    fix: remove bg

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -7,7 +7,6 @@ export default function RootLayout({
 }>) {
 	return (
 		<div className="relative h-[90vh] ">
-			<div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-[#111] bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
 			{children}
 			<Footer />
 		</div>
